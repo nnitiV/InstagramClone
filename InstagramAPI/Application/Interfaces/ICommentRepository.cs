faf4cb32c7b212644120f6bb4b5ef7f9050976cf
nnitivv <vihstudent@gmail.com>
2026-02-01 18:20:50 -0300

    Started creating Comment repository, changing to comment branch

---
+++ InstagramAPI/Application/Interfaces/ICommentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface ICommentRepository
+    {
+        Task<List<Comment>> GetAllCommentsFromPostAsync(int postId);
+        Task<Comment> GetCommentByIdAsync(int commentId);
+        Task AddCommentAsync(Comment comment);
+        Task<bool> DeleteCommentAsync(int commentId, int userId);
+        Task<bool> UpdateCommentAsync(Comment comment, int userId);
+    }
+}
---
+++ InstagramAPI/Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class CommentRepository: ICommentRepository
+    {
+        private readonly AppDbContext _context;
+        public CommentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Comment>> GetAllCommentsFromPostAsync(int postId)
+        {
+            return await _context.Comments.Where(c => c.PostId == postId).ToListAsync();
+        }
+    }
+}
