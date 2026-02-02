4b548e7fa697d91ffc99eb05b582960a6636b6da
nnitivv <vihstudent@gmail.com>
2026-02-02 18:57:16 -0300

    Implemented follower repository

---
+++ InstagramAPI/Application/Interfaces/IFollowerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IFollowerRepository
+    {
+        Task<int> GetTotalFollowersAsync(int userId);
+        Task<int> GetTotalFollowingAsync(int userId);
+        Task FollowUserAsync(Follower follower);
+        Task<bool> UnfollowUserAsync(int followingUserId, int followedUserId);
+        Task<bool> IsFollowingAsync(int followingUserId, int followedUserId);
+    }
+}
---
+++ InstagramAPI/Infrastructure/Repositories/FollowerRepository.cs
@@ -0,0 +1,42 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class FollowerRepository : IFollowerRepository
+    {
+        private readonly AppDbContext _context;
+        public FollowerRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task FollowUserAsync(Follower follower)
+        {
+            await _context.Followers.AddAsync(follower);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> GetTotalFollowersAsync(int userId)
+        {
+            return await _context.Followers.CountAsync(f => f.UserIdFollowed == userId);
+        }
+
+        public async Task<int> GetTotalFollowingAsync(int userId)
+        {
+            return await _context.Followers.CountAsync(f => f.UserIdFollowing == userId);
+        }
+
+        public async Task<bool> IsFollowingAsync(int observerId, int targetId)
+        {
+            return await _context.Followers.AnyAsync(f => f.UserIdFollowing == observerId && f.UserIdFollowed == targetId);
+        }
+
+        public async Task<bool> UnfollowUserAsync(int followingUserId, int followedUserId)
+        {
+            return await _context.Followers.Where(f => f.UserIdFollowing == followingUserId && f.UserIdFollowed == followedUserId).ExecuteDeleteAsync() > 0;
+        }
+    }
+}
