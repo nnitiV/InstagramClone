4e69291095f0f78f0a5f24930ba95d9a096c3b0f
nnitivv <vihstudent@gmail.com>
2026-02-02 14:54:51 -0300

    Comment Like Repository

---
+++ InstagramAPI/Application/Interfaces/ICommentLikeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface ICommentLikeRepository
+    {
+        Task<int> GetAmountOfLikesAsync(int commentId);
+        Task LikeCommentAsync(CommentLike commentLike);
+        Task<bool> UnlikeCommentAsync(int commentId, int userId);
+        Task<bool> HasUserLikedItAsync(int commentId, int userId);
+    }
+}
---
+++ InstagramAPI/Infrastructure/Repositories/CommentLikeRepository.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class CommentLikeRepository: ICommentLikeRepository
+    {
+        private readonly AppDbContext _context;
+        public CommentLikeRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> GetAmountOfLikesAsync(int commentId)
+        {
+            return await _context.CommentLikes.CountAsync(cl => cl.CommentId == commentId);
+        }
+
+        public async Task<bool> HasUserLikedItAsync(int commentId, int userId)
+        {
+            return await _context.CommentLikes.AnyAsync(cl => cl.UserId == userId && cl.CommentId == commentId);
+        }
+
+        public async Task LikeCommentAsync(CommentLike commentLike)
+        {
+            await _context.CommentLikes.AddAsync(commentLike);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<bool> UnlikeCommentAsync(int commentId, int userId)
+        {
+            return await _context.CommentLikes.Where(cl => cl.UserId == userId && cl.CommentId == commentId).ExecuteDeleteAsync() > 0;
+        }
+    }
+}
