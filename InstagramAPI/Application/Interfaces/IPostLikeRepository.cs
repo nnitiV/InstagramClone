ce2cd04e2f2007f157445d8f29f4c7a66baccb8f
nnitivv <vihstudent@gmail.com>
2026-02-02 15:40:10 -0300

    Implemented post like repository

---
+++ InstagramAPI/Application/Interfaces/IPostLikeRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IPostLikeRepository
+    {
+        Task<int> GetAmountOfLikesAsync(int postId);
+        Task LikePostAsync(PostLike postLike);
+        Task<bool> UnlikePostAsync(int postId, int userId);
+        Task<bool> HasUserLikedItAsync(int postId, int userId);
+    }
+}
