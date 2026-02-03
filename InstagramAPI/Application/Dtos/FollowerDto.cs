b70bacf1db159d6d399aa8975987b41f72df54cf
nnitivv <vihstudent@gmail.com>
2026-02-03 07:51:22 -0300

    Added name to user entity, updated database and added follower dto

---
+++ InstagramAPI/Application/Dtos/FollowerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Dtos
+{
+    public class FollowerDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string ProfilePictureUrl { get; set; } = string.Empty;
+    }
+}
--- InstagramAPI/Domain/Entities/User.cs
+++ InstagramAPI/Domain/Entities/User.cs
@@ -4,6 +4,7 @@ namespace Domain.Entities
 {
     public class User : BaseEntity
     {
+        public string Name { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
