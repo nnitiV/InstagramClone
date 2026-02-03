b6491e7e7a59d5b4cb1e02efecf68b05a24904bf
nnitivv <vihstudent@gmail.com>
2026-02-03 13:42:06 -0300

    Added message controller

---
+++ InstagramAPI/API/Controllers/MessageController.cs
@@ -0,0 +1,34 @@
+﻿using API.Extensions;
+using Application.Dtos;
+using Application.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/messages")]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessageService _messageService;
+        public MessageController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+        [HttpGet("{otherUserId}")]
+        public async Task<IActionResult> GetChatHistory(int otherUserId)
+        {
+            var currentUserId = User.GetUserId();
+            var messages = await _messageService.GetChatHistoryAsync(currentUserId, otherUserId);
+            return Ok(new { messages = messages });
+        }
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(SendMessageDto sendMessageDto)
+        {
+            var currentUserId = User.GetUserId();
+            var message = await _messageService.SendMessageAsync(currentUserId, sendMessageDto);
+            return Ok(new { message = message });
+        }
+    }
+}
