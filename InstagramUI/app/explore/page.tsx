f69b31ccc38346e7008f3125776680dd2d0fc86d
nnitivv <vihstudent@gmail.com>
2026-02-04 15:20:55 -0300

    Added placeholder for other pages

---
+++ InstagramUI/app/explore/page.tsx
@@ -0,0 +1,8 @@
+export default function SearchPage() {
+    return (
+        <div className="vh-100 d-flex justify-content-center align-items-center">
+            <h1>Explore</h1>
+        </div>
+    )
+};
+
