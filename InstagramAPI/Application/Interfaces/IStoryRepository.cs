f060d814b3b37b134f50df2e803bbe166e7218d8
nnitivv <vihstudent@gmail.com>
2026-02-04 12:11:20 -0300

    Implemented story repository

---
+++ InstagramAPI/Application/Interfaces/IStoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IStoryRepository
+    {
+        Task CreateStoryAsync(Story story);
+        Task<IEnumerable<Story>> GetActiveStoriesAsync();
+    }
+}
---
+++ InstagramAPI/Infrastructure/Repositories/StoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class StoryRepository: IStoryRepository
+    {
+        private readonly AppDbContext _context;
+        public StoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateStoryAsync(Story story)
+        {
+            _context.Stories.Add(story);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Story>> GetActiveStoriesAsync()
+        {
+            return await _context.Stories
+                .Where(s => s.ExpiresAt > DateTimeOffset.UtcNow)
+                .Include(s => s.User) 
+                .OrderByDescending(s => s.CreatedAt) 
+                .ToListAsync();
+        }
+    }
+}
