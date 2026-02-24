5d9dcbf1357d9fe6ac0532b70f747b820f881c85
Vitor Augusto Elias Cunha <vihstudent@gmail.com>
2026-02-24 20:15:51 -0300

    Filtering between today follows and earlier follows

--- InstagramUI/components/SearchOffset.tsx
+++ InstagramUI/components/SearchOffset.tsx
@@ -12,7 +12,7 @@ const MOCK_NOTIFICATIONS_API: Notification[] = [
         id: 1,
         username: "user_alpha",
         profilePictureUrl: "https://i.pravatar.cc/150?u=1",
-        timestamp: new Date().toISOString(),
+        timestamp: new Date().toDateString(),
         followedUserId: null
     },
     {
@@ -45,6 +45,8 @@ const MOCK_NOTIFICATIONS_API: Notification[] = [
     }
 ];
 export default function SearchOffset() {
+    const todayFollows = MOCK_NOTIFICATIONS_API.filter(n => new Date(n.timestamp).toDateString() == new Date().toDateString());
+    const earlier = MOCK_NOTIFICATIONS_API.filter(n => new Date(n.timestamp).toDateString() != new Date().toDateString());
     return (
         <div className="offcanvas offcanvas-start px-3 w-50" tabIndex={-1} id="notificationsOffset" aria-labelledby="notificationsOffsetLabel">
             <div className="d-flex flex-column overflow-y-auto">
@@ -52,34 +54,70 @@ export default function SearchOffset() {
                     <h5 className="offcanvas-title" id="notificationsOffsetLabel">Notifications</h5>
                     <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
-                <h1 className="mt-4 fw-bold fs-5 p-0 w-100">Today</h1>
-                {MOCK_NOTIFICATIONS_API.map(user => (
-                <div className="transparent-background-hover cursor-pointer rounded w-100 d-flex justify-content-between align-items-center px-2">
-                    <Link href={`#`} className="text-decoration-none text-body">
-                        <div className="d-flex mb-1 p-0 py-2" data-bs-dismiss="offcanvas"
-                            data-mdb-ripple-init
-                            data-mdb-ripple-color="light">
-                            <div className="p-1 rounded-circle position-relative me-2">
-                                <img
-                                    src={user.profilePictureUrl ? user.profilePictureUrl : "https://www.shutterstock.com/shutterstock/photos/2286554497/display_1500/stock-photo-random-pictures-cute-and-funny-2286554497.jpg"}
-                                    alt="Story"
-                                    className="rounded-circle"
-                                    style={{ width: "46px", height: "46px", objectFit: "cover", }}
-                                />
+                {todayFollows.length >= 0 && (
+                    <>
+                        <h1 className="mt-4 fw-bold fs-5 p-0 w-100">Today</h1>
+                        {todayFollows.map(user => (
+                            <div className="transparent-background-hover cursor-pointer rounded w-100 d-flex justify-content-between align-items-center px-2">
+                                <Link href={`#`} className="text-decoration-none text-body">
+                                    <div className="d-flex mb-1 p-0 py-2" data-bs-dismiss="offcanvas"
+                                        data-mdb-ripple-init
+                                        data-mdb-ripple-color="light">
+                                        <div className="p-1 rounded-circle position-relative me-2">
+                                            <img
+                                                src={user.profilePictureUrl ? user.profilePictureUrl : "https://www.shutterstock.com/shutterstock/photos/2286554497/display_1500/stock-photo-random-pictures-cute-and-funny-2286554497.jpg"}
+                                                alt="Story"
+                                                className="rounded-circle"
+                                                style={{ width: "46px", height: "46px", objectFit: "cover", }}
+                                            />
+                                        </div>
+                                        <div className="d-flex flex-column justify-content-center" style={{ fontSize: ".85rem" }}>
+                                            <p className="m-0 p-0">{user.username} started following you <span>jan, 12</span></p>
+                                        </div>
+                                    </div>
+                                </Link>
+                                {user.followedUserId != null
+                                    ?
+                                    <button type="button" className="btn btn- ms-3" style={{ height: "fit-content", fontSize: ".75em" }}>Unfollow</button>
+                                    :
+                                    <button type="button" className="btn btn-primary ms-1" style={{ height: "fit-content", fontSize: ".75em" }}>Follow back</button>
+                                }
                             </div>
-                            <div className="d-flex flex-column justify-content-center w-75" style={{ fontSize: ".85rem" }}>
-                                <p className="m-0 p-0">{user.username} started following you <span>jan, 12</span> - {user.followedUserId}</p>
+                        ))}
+                    </>
+                )}
+                {earlier.length >= 0 && (
+                    <>
+                        <h1 className="mt-4 fw-bold fs-5 p-0 w-100">Earlier</h1>
+                        {earlier.map(user => (
+                            <div className="transparent-background-hover cursor-pointer rounded w-100 d-flex justify-content-between align-items-center px-2">
+                                <Link href={`#`} className="text-decoration-none text-body">
+                                    <div className="d-flex mb-1 p-0 py-2" data-bs-dismiss="offcanvas"
+                                        data-mdb-ripple-init
+                                        data-mdb-ripple-color="light">
+                                        <div className="p-1 rounded-circle position-relative me-2">
+                                            <img
+                                                src={user.profilePictureUrl ? user.profilePictureUrl : "https://www.shutterstock.com/shutterstock/photos/2286554497/display_1500/stock-photo-random-pictures-cute-and-funny-2286554497.jpg"}
+                                                alt="Story"
+                                                className="rounded-circle"
+                                                style={{ width: "46px", height: "46px", objectFit: "cover", }}
+                                            />
+                                        </div>
+                                        <div className="d-flex flex-column justify-content-center" style={{ fontSize: ".85rem" }}>
+                                            <p className="m-0 p-0">{user.username} started following you <span>jan, 12</span></p>
+                                        </div>
+                                    </div>
+                                </Link>
+                                {user.followedUserId != null
+                                    ?
+                                    <button type="button" className="btn btn- ms-3" style={{ height: "fit-content", fontSize: ".75em" }}>Unfollow</button>
+                                    :
+                                    <button type="button" className="btn btn-primary ms-1" style={{ height: "fit-content", fontSize: ".75em" }}>Follow back</button>
+                                }
                             </div>
-                        </div>
-                    </Link>
-                    {user.followedUserId != null
-                        ?
-                        <button type="button" className="btn btn- ms-3" style={{height: "fit-content", fontSize: ".75em"}}>Unfollow</button>
-                        :
-                        <button type="button" className="btn btn-primary ms-1" style={{height: "fit-content", fontSize: ".75em"}}>Follow back</button>
-                    }
-                </div>
-                ))}
+                        ))}
+                    </>
+                )}
             </div >
         </div >
     )
