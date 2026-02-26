1a73f1417d5ad2267db41285f5067b44c4f99536
Vitor Augusto Elias Cunha <vihstudent@gmail.com>
2026-02-26 08:13:15 -0300

    Fixed story position

--- InstagramUI/feature/story/components/StoryPreview.tsx
+++ InstagramUI/feature/story/components/StoryPreview.tsx
@@ -1,29 +1,49 @@
 import { Story } from "@/types/feed";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 type StoryPreviewProps = {
   story: Story | undefined;
   storyPosition: number;
 }
 export default function StoryPreview({ story, storyPosition }: StoryPreviewProps) {
+  function useWindowSize() {
+    const [width, setWidth] = useState(typeof window !== "undefined" ? window.innerWidth : 1200);
+
+    useEffect(() => {
+      const handleResize = () => setWidth(window.innerWidth);
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return width;
+  }
+
+  const width = useWindowSize();
+
   const getOffset = () => {
+
+    const farOffset = width * 0.2;
+    const nearOffset = width * 0.05;
     switch (storyPosition) {
       case 1:
-        return { right: 400 };
+        return { right: nearOffset };
       case 2:
-        return { right: 100 };
+        return { right: farOffset };
       case -1:
-        return { left: 400 };
+        return { left: nearOffset };
       case -2:
-        return { left: 100 };
+        return { left: farOffset };
+      default:
+        return {};
     }
   }
   const storyPositionOffset = getOffset();
   return (
-    <Link href={`/stories/${story?.username}/${story?.id}`} className="card p-0 m-0 border-0 position-absolute text-decoration-none" 
+    <Link href={`/stories/${story?.username}/${story?.id}`} className="card p-0 m-0 border-0 position-absolute text-decoration-none"
       style={{
         ...storyPositionOffset,
-        width: "13vw", height: "50vh", backgroundImage: `url(${story?.mediaurl != null ? story.mediaurl :'https://wallpapers.com/images/featured/anime-iphone-psdmm565oizldbbg.jpg'})`,
+        width: "13vw", height: "50vh", backgroundImage: `url(${story?.mediaurl != null ? story.mediaurl : 'https://wallpapers.com/images/featured/anime-iphone-psdmm565oizldbbg.jpg'})`,
         backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center"
       }}>
       <div className="card-body text-white d-flex flex-column align-items-center justify-content-center" style={{ background: "rgba(25,25,25,0.5)" }}>
