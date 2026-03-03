338a54710d79b5fb8d759d172d096418df7bc4d1
Vitor Augusto Elias Cunha <vihstudent@gmail.com>
2026-03-03 18:48:21 -0300

    Small fix on the menu navbar

--- InstagramUI/components/layout/Sidebar.tsx
+++ InstagramUI/components/layout/Sidebar.tsx
@@ -38,7 +38,7 @@ export default function Sidebar({ children, picture }: { children: ReactNode, pi
           </Link>
           <ul className="dropdown-menu text-small shadow">
             <li><Link className="dropdown-item" href="#">Settings</Link></li>
-            <li><Link className="dropdown-item" href="#">Profile</Link></li>
+            <li><Link className="dropdown-item" href="/profile">Profile</Link></li>
             <li><hr className="dropdown-divider" /></li>
             <li><Link onClick={handleLogout} className="dropdown-item" href="#">Sign out</Link></li>
           </ul>
