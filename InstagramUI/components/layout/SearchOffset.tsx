92df3b8832ffe665f25d323ba672a2cb929dfeee
Vitor Augusto Elias Cunha <vihstudent@gmail.com>
2026-03-07 09:41:40 -0300

    Now there's loading spinner when searching

--- InstagramUI/components/layout/SearchOffset.tsx
+++ InstagramUI/components/layout/SearchOffset.tsx
@@ -1,7 +1,7 @@
 import { searchUsers } from "@/services/search.service";
 import { UserProfile } from "@/types/user";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 function useDebounce<T>(value: T, delay: number) {
     const [debounceValue, setDebounceValue] = useState<T>(value);
@@ -12,25 +12,28 @@ function useDebounce<T>(value: T, delay: number) {
         }, delay);
         return () => clearTimeout(handler);
     }, [value, delay]);
-
     return debounceValue;
 }
 
 export default function SearchOffset() {
     const [searchText, setSearchText] = useState<string>("");
+    const [isSearching, setIsSearching] = useState<boolean>(false);
     const [users, setUsers] = useState<UserProfile[]>([]);
     const debounceSearch = useDebounce(searchText, 500);
 
     useEffect(() => {
+        setIsSearching(true);
         const fetchUserList = async () => {
             if (debounceSearch) {
                 const usersRes = await searchUsers(debounceSearch);
                 console.log("Res:", usersRes);
                 setUsers(usersRes.result);
+                setIsSearching(false);
             }
         }
         fetchUserList();
-    }, [debounceSearch])
+    }, [debounceSearch]);
+    
     return (
         <div className="offcanvas offcanvas-start" tabIndex={-1} id="searchOffcanvas" aria-labelledby="searchOffcanvasLabel">
             <div className="offcanvas-header d-flex flex-column">
@@ -53,29 +56,34 @@ export default function SearchOffset() {
                         </div>
                     </>
                     :
-                    users.length > 0 ?
-                        users.map(user => (
-                            <Link href={`/profile/${user.username}`} key={user.id} className="text-decoration-none text-body" onClick={() => setSearchText("")}>
-                                <div className="d-flex mb-1 cursor-pointer transparent-background-hover rounded px-2 py-2" data-bs-dismiss="offcanvas"
-                                    data-mdb-ripple-init
-                                    data-mdb-ripple-color="light">
-                                    <div className="p-1 rounded-circle position-relative me-2">
-                                        <img
-                                            src={!user.profilePictureUrl ? "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" : "http://localhost:5000/" + user.profilePictureUrl}
-                                            alt="Story"
-                                            className="rounded-circle"
-                                            style={{ width: "46px", height: "46px", objectFit: "cover", }}
-                                        />
-                                    </div>
-                                    <div className="d-flex flex-column justify-content-center" style={{ fontSize: "14px" }}>
-                                        <p className="m-0 p-0">{user.username}</p>
-                                        <p className="m-0 p-0" style={{ color: "rgba(75,75,75,0.75)" }}>{user.name} - {user.followersCount}</p>
-                                    </div>
-                                </div>
-                            </Link>
-                        ))
+                    isSearching ?
+                        <div className="spinner-border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
                         :
-                        <p>No user yet</p>
+                        users.length > 0 ?
+                            users.map(user => (
+                                <Link href={`/profile/${user.username}`} key={user.id} className="text-decoration-none text-body" onClick={() => setSearchText("")}>
+                                    <div className="d-flex mb-1 cursor-pointer transparent-background-hover rounded px-2 py-2" data-bs-dismiss="offcanvas"
+                                        data-mdb-ripple-init
+                                        data-mdb-ripple-color="light">
+                                        <div className="p-1 rounded-circle position-relative me-2">
+                                            <img
+                                                src={!user.profilePictureUrl ? "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" : "http://localhost:5000/" + user.profilePictureUrl}
+                                                alt="Story"
+                                                className="rounded-circle"
+                                                style={{ width: "46px", height: "46px", objectFit: "cover", }}
+                                            />
+                                        </div>
+                                        <div className="d-flex flex-column justify-content-center" style={{ fontSize: "14px" }}>
+                                            <p className="m-0 p-0">{user.username}</p>
+                                            <p className="m-0 p-0" style={{ color: "rgba(75,75,75,0.75)" }}>{user.name} - {user.followersCount}</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            ))
+                            :
+                            <p>No user yet</p>
                 }
             </div >
         </div >
