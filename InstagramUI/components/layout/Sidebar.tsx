a76c8f3bb57d0bc9b38f0050ed8a4de21899dd82
nnitivv <vihstudent@gmail.com>
2026-03-26 21:37:44 -0300

    Test height navbar fix

--- InstagramUI/components/layout/Sidebar.tsx
+++ InstagramUI/components/layout/Sidebar.tsx
@@ -9,39 +9,90 @@ import CreateStoryModal from "./CreateStoryModal";
 import InstagramLogo from "../ui/InstagramLogo";
 import SidebarMoreDropdown from "./SidebarMoreDropdown";
 
-type SidebarProps = { children: ReactNode, picture: string };
+type SidebarProps = { children: ReactNode; picture: string };
 
 const SIDEBAR_LINKS = [
   { url: "/", icon: "house", text: "Home", target: "", isModal: false },
   // { url: "/reels", icon: "camera-reels", text: "Reels",target: "", isModal: false  },
-  { url: "/messages", icon: "chat-right", text: "Messages", target: "", isModal: false },
-  { url: "#", icon: "search", text: "Search", target: "searchOffcanvas", isModal: false, },
-  { url: "/explore", icon: "compass", text: "Explore", target: "", isModal: false },
-  { url: "#", icon: "heart", text: "Notifications", target: "notificationsOffset", isModal: false },
   {
-    url: "#", icon: "plus-square", text: "Create", target: "createModal", isModal: true, isDropdown: true,
-    dropdownOptions: ["Post", "Story"], dropdownTargets: ["createModal", "createStory"],
+    url: "/messages",
+    icon: "chat-right",
+    text: "Messages",
+    target: "",
+    isModal: false,
   },
-  { url: "/profile", icon: "person-circle", text: "Profile", target: "", isModal: false },
+  {
+    url: "#",
+    icon: "search",
+    text: "Search",
+    target: "searchOffcanvas",
+    isModal: false,
+  },
+  {
+    url: "/explore",
+    icon: "compass",
+    text: "Explore",
+    target: "",
+    isModal: false,
+  },
+  {
+    url: "#",
+    icon: "heart",
+    text: "Notifications",
+    target: "notificationsOffset",
+    isModal: false,
+  },
+  {
+    url: "#",
+    icon: "plus-square",
+    text: "Create",
+    target: "createModal",
+    isModal: true,
+    isDropdown: true,
+    dropdownOptions: ["Post", "Story"],
+    dropdownTargets: ["createModal", "createStory"],
+  },
+  {
+    url: "/profile",
+    icon: "person-circle",
+    text: "Profile",
+    target: "",
+    isModal: false,
+  },
 ];
 
 export default function Sidebar({ children, picture }: SidebarProps) {
   return (
-    <div className="d-flex vh-100 overflow-hidden">
+    <div className="d-flex vh-100 overflow-hidden" style={{ height: "100dvh" }}>
       <div
         className="d-flex flex-column justify-content-between flex-shrink-0 shadow-sm px-2 py-3 border-end"
         style={{ width: "fit-content" }}
       >
-        <Link href="/" className="transparent-background-hover d-flex align-items-center me-md-auto text-body 
-        text-decoration-none p-2 fs-5">
+        <Link
+          href="/"
+          className="transparent-background-hover d-flex align-items-center me-md-auto text-body 
+        text-decoration-none p-2 fs-5"
+        >
           <InstagramLogo isIcon={true} />
         </Link>
 
-        <ul className="nav nav-pills flex-column align-items-center align-items-sm-start w-100" id="menu">
-          {SIDEBAR_LINKS.map(item => (
-            <SidebarItem key={item.text} url={item.url} iconName={item.icon} text={item.text}
-              targetModal={item.target} isModal={item.isModal} isDropdown={item.isDropdown} dropdownOptions={item.dropdownOptions}
-              dropdownTargets={item.dropdownTargets} picture={item.text === "Profile" ? picture : undefined} />
+        <ul
+          className="nav nav-pills flex-column align-items-center align-items-sm-start w-100"
+          id="menu"
+        >
+          {SIDEBAR_LINKS.map((item) => (
+            <SidebarItem
+              key={item.text}
+              url={item.url}
+              iconName={item.icon}
+              text={item.text}
+              targetModal={item.target}
+              isModal={item.isModal}
+              isDropdown={item.isDropdown}
+              dropdownOptions={item.dropdownOptions}
+              dropdownTargets={item.dropdownTargets}
+              picture={item.text === "Profile" ? picture : undefined}
+            />
           ))}
         </ul>
 
@@ -49,9 +100,7 @@ export default function Sidebar({ children, picture }: SidebarProps) {
       </div>
 
       {/* Page content */}
-      <div className="flex-grow-1 overflow-y-auto">
-        {children}
-      </div>
+      <div className="flex-grow-1 overflow-y-auto">{children}</div>
 
       {/* Modals */}
       <CreatePostModal />
@@ -60,4 +109,4 @@ export default function Sidebar({ children, picture }: SidebarProps) {
       <NotificationsOffcanvas />
     </div>
   );
-};
+}
