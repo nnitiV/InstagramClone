07e9fd1845932e5e64e5b596e00f378c98af5ebf
nnitivv <vihstudent@gmail.com>
2026-03-28 10:45:44 -0300

    Forgot about the migration

---
+++ InstagramAPI/Infrastructure/Migrations/20260328134428_NotificationDependency.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NotificationDependency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_PostId",
+                table: "Notifications",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_StoryId",
+                table: "Notifications",
+                column: "StoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Posts_PostId",
+                table: "Notifications",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notifications_Stories_StoryId",
+                table: "Notifications",
+                column: "StoryId",
+                principalTable: "Stories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Posts_PostId",
+                table: "Notifications");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notifications_Stories_StoryId",
+                table: "Notifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notifications_PostId",
+                table: "Notifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notifications_StoryId",
+                table: "Notifications");
+        }
+    }
+}
